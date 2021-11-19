fabd6a1a31e8119eeb71d181bdc25e9cb1db658f
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2021-11-19 23:50:07 +0330

    update

--- A/settings.py
+++ A/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -11,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$!@)5d-8&y=_xz0(*3_*v-p-y3&_em19oqn(34f0%-2m_#af&l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -75,13 +76,13 @@ WSGI_APPLICATION = 'A.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'postgres',
-        'PORT': '5432',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('_DB_NAME', 'djangotest'),
+        'USER': os.getenv('_DB_USER', 'root'),
+        'PASSWORD': os.getenv('_DB_PASS', 'root'),
+        'HOST': os.getenv('_DB_HOST', '127.0.0.1'),
+        'PORT': os.getenv('_DB_PORT', '3306')
+    },
 }
 
 
@@ -127,9 +128,9 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 # ARVAN CLOUD STORAGE
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = 'your access key id'
-AWS_SECRET_ACCESS_KEY = 'your secret access key'
-AWS_STORAGE_BUCKET_NAME = 'your bucket name'
+AWS_ACCESS_KEY_ID = '3ffce2b4-b922-48cb-9545-14fa79f63028'
+AWS_SECRET_ACCESS_KEY = 'ba07dc836ef91ba7fc22f45214680ef5223a38c59e5e9731c88735192d561384'
+AWS_STORAGE_BUCKET_NAME = 'sajjadtest'
 AWS_SERVICE_NAME = 's3'
 AWS_S3_ENDPOINT_URL = 'https://s3.ir-thr-at1.arvanstorage.com'
 AWS_S3_FILE_OVERWRITE = False
